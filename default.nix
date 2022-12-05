b1f1a3d2bbebd5b6802362f9acb3c285d48c48bf
Florian Paul Schmidt <mista.tapas@gmx.net>
2022-12-05 09:32:39 +0100

    remove some cruft

--- default.nix
+++ default.nix
@@ -4,19 +4,7 @@ buildPythonPackage rec {
   pname = "regexec";
   version = "1.0.0";
 
-  # If you have your sources locally, you can specify a path
   src = ./.;
-
-  # Pull source from a Git server. Optionally select a specific `ref` (e.g. branch),
-  # or `rev` revision hash.
-  # src = builtins.fetchGit {
-  #   url = "git://github.com/stigok/ruterstop.git";
-  #   ref = "master";
-  #   #rev = "a9a4cd60e609ed3471b4b8fac8958d009053260d";
-  # };
-
-  # Specify runtime dependencies for the package
-  # propagatedBuildInputs = with pkgs; [ bottle jalv ogfx-tools mod-host mod-utilities ];
 
   doCheck = false;
 
